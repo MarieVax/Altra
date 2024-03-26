ee4deddb1c8be86c26d6c370784f7c42c8e27e28
Yvonne Reyes <panganibanyvonne.yp@gmail.com>
2024-03-26 10:40:55 +0800

    Update services-post-type.php
    Services block custom post type

--- wordpress/themes/altra/inc/custom-post-types/services-post-type.php
+++ wordpress/themes/altra/inc/custom-post-types/services-post-type.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Service custom post types declaration
+ * service custom post types declaration
  *
  * @since 1.0
  */
@@ -14,16 +14,16 @@ function service_post_type()
     'name_admin_bar'        => __('Services', 'altra'),
     'menu_name'             => __('Services', 'altra'),
     'singular_name'         => __('Service', 'altra'),
-    'add_new'               => __('Add New', 'service', 'altra'),
-    'add_new_item'          => __('Add New service', 'altra'),
-    'new_item'              => __('New', 'service', 'altra'),
-    'edit_item'             => __('Edit service', 'altra'),
-    'view_items'            => __('View services', 'altra'),
-    'view_item'             => __('View service', 'altra'),
-    'search_items'          => __('Search service', 'altra'),
-    'all_items'             => __('All services', 'altra'),
-    'not_found'             => __('No service found', 'altra'),
-    'not_found_in_trash'    => __('No service found in trash', 'altra'),
+    'add_new'               => __('Add New', 'Service', 'altra'),
+    'add_new_item'          => __('Add New Service', 'altra'),
+    'new_item'              => __('New', 'Service', 'altra'),
+    'edit_item'             => __('Edit Service', 'altra'),
+    'view_items'            => __('View Services', 'altra'),
+    'view_item'             => __('View Service', 'altra'),
+    'search_items'          => __('Search Service', 'altra'),
+    'all_items'             => __('All Services', 'altra'),
+    'not_found'             => __('No Service found', 'altra'),
+    'not_found_in_trash'    => __('No Service found in trash', 'altra'),
   );
 
   $args = array(
@@ -37,7 +37,7 @@ function service_post_type()
     'has_archive'        => true,
     'hierarchical'       => true,
     'menu_position'      => null,
-    'menu_icon'          => 'dashicons-images-alt',
+    'menu_icon'          => 'dashicons-products',
     'exclude_from_search' => false,
     'show_in_rest' => false,
     'supports'           => array('title', 'editor', 'thumbnail', 'custom-fields', 'excerpt'),
@@ -50,8 +50,8 @@ add_action('init', 'service_post_type', 0);
 
 if (function_exists('acf_add_options_sub_page')) {
   acf_add_options_sub_page(array(
-    'page_title'   => 'services Settings',
-    'menu_title'  => 'services Settings',
-    'parent_slug'  => 'edit.php?post_type=services',
+    'page_title'   => 'Services Settings',
+    'menu_title'  => 'Services Settings',
+    'parent_slug'  => 'edit.php?post_type=service',
   ));
 }
